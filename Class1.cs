a1624719ad5f964e7773e9c227aae1e9b170e33f
smilingLeeEnfield <159907717+smilingLeeEnfield@users.noreply.github.com>
2025-10-21 10:19:33 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oresundbron
+{
+    public class OresundCar : bridge.Car
+    {
+        public OresundCar(string LP, bool biz) : base(LP, biz)
+        {
+        }
+        /// <summary>
+        /// returns price of a car ticket on the Øresund bridge with a discount if brobizz is used
+        /// </summary>
+        /// <returns></returns>
+        public override double Price()
+        {
+            if (brobizz) return 178;
+            return 460;
+        }
+    }
+}
---
+++ OresundMC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oresundbron
+{
+    public class OresundMC : bridge.MC
+    {
+        public OresundMC(string LP, bool biz) : base(LP, biz)
+        {
+        }
+        /// <summary>
+        /// returns price of a MC ticket on the Øresund bridge with a discount if brobizz is used
+        /// </summary>
+        /// <returns></returns>
+        public override double Price()
+        {
+            if (brobizz) return 92;
+            return 235;
+        }
+    }
+}
